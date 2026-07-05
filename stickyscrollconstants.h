e2ed530783a5f5c0c00f70725177f34e3790390f
Petr Mironychev <9195189+Palm1r@users.noreply.github.com>
2026-07-05 10:23:45 +0200

    init: Plugin is created

---
+++ stickyscrollconstants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace StickyScroll::Constants {
+
+const char ACTION_ID[] = "StickyScroll.Action";
+const char MENU_ID[] = "StickyScroll.Menu";
+
+} // namespace StickyScroll::Constants
